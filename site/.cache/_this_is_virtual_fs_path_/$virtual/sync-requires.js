a9da3ec8d67188c9dbf1cf60438bb0b70e1a9be1
Michael Fudge <mafudge@gmail.com>
2021-11-24 16:33:22 -0500

    publish website

---
+++ site/.cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -0,0 +1,10 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---src-pages-404-js": preferDefault(require("C:\\Users\\mafud\\Documents\\git\\mafudge\\static-website-michaelfudge.com\\site\\src\\pages\\404.js")),
+  "component---src-pages-index-js": preferDefault(require("C:\\Users\\mafud\\Documents\\git\\mafudge\\static-website-michaelfudge.com\\site\\src\\pages\\index.js"))
+}
+
